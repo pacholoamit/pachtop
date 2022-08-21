1ade7d78a55df5685ff2d80bdd8eaa317be1afe0
pacholoamit <pacholoamit.tech@gmail.com>
2022-08-21 18:43:37 +0800

    remove window_vibrancy

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,24 +3,9 @@
     windows_subsystem = "windows"
 )]
 use app::{models, Metrics};
-use tauri::Manager;
-use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
 
 fn main() {
     tauri::Builder::default()
-        .setup(|a| {
-            let window = a.get_window("main").unwrap();
-
-            #[cfg(target_os = "macos")]
-            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow)
-                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
-
-            #[cfg(target_os = "windows")]
-            apply_blur(&window, Some((18, 18, 18, 125)))
-                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
-
-            Ok(())
-        })
         .invoke_handler(tauri::generate_handler![memory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
