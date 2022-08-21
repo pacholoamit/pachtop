85b9eac6d2fc84688e0b0fbea5595051b95b2b35
pacholoamit <pacholoamit.tech@gmail.com>
2022-08-21 14:20:40 +0800

    Change function syntax of App to arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import AppProvider from "@/providers";
 import AppRoutes from "@/routes";
 
-function App() {
+const App = () => {
   return (
     <AppProvider>
       <AppRoutes />
     </AppProvider>
   );
-}
+};
 
 export default App;
