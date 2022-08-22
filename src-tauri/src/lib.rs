e1a21cad740ccad53ab4628378cc0cce0eeef4b4
pacholoamit <pacholoamit.tech@gmail.com>
2022-08-22 08:59:07 +0800

    Temporarily remove unrequired data

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use systemstat::{
     BTreeMap, CPULoad, DelayedMeasurement, Filesystem, Network, Platform, Swap, System,
 };
+
 pub struct Metrics {
     pub sys: systemstat::platform::PlatformImpl,
 }
@@ -34,40 +35,5 @@ impl Metrics {
         };
 
         Ok(mem)
-    }
-
-    pub fn mounts(&self) -> Vec<Filesystem> {
-        match self.sys.mounts() {
-            Ok(mounts) => mounts,
-            Err(e) => panic!("\nMounts error: {}", e),
-        }
-    }
-
-    pub fn networks(&self) -> BTreeMap<String, Network> {
-        match self.sys.networks() {
-            Ok(networks) => networks,
-            Err(e) => panic!("\nNetworks error: {}", e),
-        }
-    }
-
-    pub fn uptime(&self) -> Duration {
-        match self.sys.uptime() {
-            Ok(uptime) => uptime,
-            Err(e) => panic!("\nUptime error: {}", e),
-        }
-    }
-
-    pub fn swap(&self) -> Swap {
-        match self.sys.swap() {
-            Ok(swap) => swap,
-            Err(e) => panic!("\nSwap error: {}", e),
-        }
-    }
-
-    pub fn cpu_load_aggregate(&self) -> DelayedMeasurement<CPULoad> {
-        match self.sys.cpu_load_aggregate() {
-            Ok(cpu_load_aggregate) => cpu_load_aggregate,
-            Err(e) => panic!("\nCPU load aggregate error: {}", e),
-        }
     }
 }
