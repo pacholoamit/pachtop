99263db6dbd84cc27c4f4c5875d4d9ba340d5555
pacholoamit <pacholoamit.tech@gmail.com>
2022-08-29 03:03:20 +0800

    Create Area chart for Ram free

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -5,14 +5,22 @@ import { Title } from "@mantine/core";
 
 const HomePage = () => {
   const { memory } = useGetMetrics();
-  const label = `Ram Used ${memory.length ? `(${memory[0].unit})` : ""}`;
+  const unit = memory.slice(-1)[0]?.unit;
+
   return (
     <>
       <Title>RAM chart</Title>
 
       <AreaChart
-        label={label}
+        label={`Ram Used ${unit}`}
         data={memory.map((m) => m.used)}
+        labels={memory.map((m) => m.timestamp)}
+      />
+      <AreaChart
+        backgroundColor="rgb(255, 99, 132)"
+        borderColor="rgb(255, 99, 132, 0.5)"
+        label={`Ram Free ${unit}`}
+        data={memory.map((m) => m.free)}
         labels={memory.map((m) => m.timestamp)}
       />
     </>
