874a9b2faa62d2f04007644e861ac31da9b9da5c
pacholoamit <pacholoamit.tech@gmail.com>
2022-09-20 06:48:04 +0800

    Add Header but disable for now

---
+++ src/layout/components/header.tsx
@@ -0,0 +1,15 @@
+import { Grid, Group, Title } from "@mantine/core";
+
+const Header = () => {
+  return (
+    <Grid gutter={"xl"} justify="space-between" style={{ height: "90px" }}>
+      <Grid.Col span={3}>
+        <Title order={2} color={"#dce1e8"}>
+          Dashboard
+        </Title>
+      </Grid.Col>
+    </Grid>
+  );
+};
+
+export default Header;
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,18 @@
 import Navbar from "@/layout/components/navbar";
-import { AppShell, Card, useMantineTheme } from "@mantine/core";
+import Header from "@/layout/components/header";
+import {
+  AppShell,
+  Card,
+  useMantineTheme,
+  Text,
+  Box,
+  Styles,
+} from "@mantine/core";
 import { Outlet } from "react-router-dom";
 
 const Layout: React.FC = () => {
   const theme = useMantineTheme();
-  const styles = {
+  const styles: Styles<"body" | "main" | "root", never> | undefined = {
     main: {
       background: theme.colors.dark[8],
     },
@@ -14,9 +22,10 @@ const Layout: React.FC = () => {
     <AppShell
       styles={styles}
       navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
       navbar={<Navbar />}
+      padding={"xl"}
     >
+      {/* <Header /> */}
       <Card style={{ backgroundColor: theme.colors.dark[7] }} radius={"md"}>
         <Outlet />
       </Card>
