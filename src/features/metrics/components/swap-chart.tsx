19733ba83bef6eeb05eb76fcd172e1967d1abe26
pacholoamit <pacholoamit.tech@gmail.com>
2022-09-26 13:41:20 +0800

    minor fix on vars

--- src/features/metrics/components/swap-chart.tsx
+++ src/features/metrics/components/swap-chart.tsx
@@ -6,11 +6,11 @@ interface SwapChartProps extends ChartProps {}
 
 const SwapChart: React.FC<SwapChartProps> = ({ xAxisMin }) => {
   const title = "Swap Memory";
-  const labels = swap.value.map((mem) => mem.timestamp);
+  const labels = swap.value.map((swap) => swap.timestamp);
   const datasets: DatasetOptions[] = [
     {
       label: `Swap Memory Used (${swap.value.slice(-1)[0]?.unit})`,
-      data: swap.value.map((mem) => ({ x: mem.timestamp, y: mem.used })),
+      data: swap.value.map((swap) => ({ x: swap.timestamp, y: swap.used })),
       backgroundColor: "rgba(53, 162, 235, 0.45)",
       borderColor: "rgba(53, 162, 235)",
       fill: true,
