8e71a6fb897e0424c4779a579667b3d99fa9c7d7
pacholoamit <pacholoamit.tech@gmail.com>
2022-10-10 14:34:07 +0800

    Add Basic data to Disks charts

--- src/features/metrics/pages/disks.page.tsx
+++ src/features/metrics/pages/disks.page.tsx
@@ -1,9 +1,9 @@
-import Card from "@/components/card";
 import useMetricsContext from "@/features/metrics/hooks/useMetricsContext";
 import DiskAreaChart from "@/features/metrics/components/disks/disk.area-chart";
-import { Stack } from "@mantine/core";
+import { Box, Card, Center, Group, Space, Stack, Text } from "@mantine/core";
 
 const xAxisMin = Date.now() - 86400;
+
 const DisksPage = () => {
   const { disks } = useMetricsContext();
 
@@ -12,8 +12,23 @@ const DisksPage = () => {
       <h2>Disks</h2>
 
       {disks?.map((disk) => (
-        <Card style={{ height: "300px" }} key={disk.id}>
-          <DiskAreaChart key={disk.id} disk={disk} xAxisMin={xAxisMin} />
+        <Card key={disk.id} shadow="xl" p="sm" radius={"md"} withBorder>
+          <Stack spacing="xl">
+            <Box style={{ height: "300px" }}>
+              <DiskAreaChart key={disk.id} disk={disk} xAxisMin={xAxisMin} />
+            </Box>
+            <Center>
+              <Stack spacing={"xs"}>
+                <Text>Name: {disk.data.at(-1)?.name}</Text>
+                <Text>Mount Point: {disk.data.at(-1)?.mountPoint}</Text>
+              </Stack>
+              <Space style={{ width: "120px" }} />
+              <Stack spacing={"xs"}>
+                <Text>File System: {disk.data.at(-1)?.fileSystem}</Text>
+                <Text>Removable: {disk.data.at(-1)?.isRemovable}</Text>
+              </Stack>
+            </Center>
+          </Stack>
         </Card>
       ))}
     </Stack>
