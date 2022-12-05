c5d1d3ec88587f339ca8f53b2dec8e492101cea6
pacholoamit <pacholoamit.tech@gmail.com>
2022-12-05 13:41:09 +0800

    Fix processes table

--- src/features/processes/pages/processes.page.tsx
+++ src/features/processes/pages/processes.page.tsx
@@ -1,7 +1,7 @@
 import { DataTableSortStatus } from "mantine-datatable";
 import { TextInput } from "@mantine/core";
 import { IconSearch } from "@tabler/icons";
-import { Process } from "@/lib/types";
+import { Command, Process } from "@/lib/types";
 import { useState, useEffect } from "react";
 
 import sortBy from "lodash.sortby";
@@ -9,41 +9,49 @@ import KillProcessVerification from "@/features/processes/components/processes.k
 import ProcessesTable from "@/features/processes/components/processes.table";
 import useMetricsContext from "@/features/metrics/hooks/useMetricsContext";
 import PageWrapper from "@/components/page-wrapper";
+import useProcesses from "@/features/processes/hooks/useProcesses";
+import useRequestMetrics from "../../metrics/hooks/useRequestMetrics";
 
+// Rerenders happen 6 times w/o strict mode
 let renders = 0;
 
 const ProcessesPage = () => {
-  const { processes } = useMetricsContext();
-
+  const { processes } = useProcesses();
   const [query, setQuery] = useState("");
-  const [records, setRecords] = useState(sortBy(processes, "name"));
+  const [records, setRecords] = useState<Process[]>([]);
   const [selectedProcess, setSelectedProcess] = useState<Process | null>(null);
   const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
     columnAccessor: "name",
     direction: "asc",
   });
-
-  console.log(renders++);
+  console.log(++renders);
 
   useEffect(() => {
-    const filteredRecords = processes.filter((process) => {
-      const filteredName = process.name
-        .toLowerCase()
-        .includes(query.toLowerCase());
-      const filteredPid = process.pid
-        .toString()
-        .toLowerCase()
-        .includes(query.toLowerCase());
+    setRecords(processes);
+    if (!query) {
+      setRecords((prev) => {
+        const data = sortBy(prev, sortStatus.columnAccessor);
+        return sortStatus.direction === "desc" ? data.reverse() : data;
+      });
+      return;
+    }
 
-      return filteredName || filteredPid;
+    setRecords((prev) => {
+      const filteredRecords = prev.filter((process) => {
+        const filteredName = process.name
+          .toLowerCase()
+          .includes(query.toLowerCase());
+        const filteredPid = process.pid
+          .toString()
+          .toLowerCase()
+          .includes(query.toLowerCase());
+
+        return filteredName || filteredPid;
+      });
+      const sorted = sortBy(filteredRecords, sortStatus.columnAccessor);
+      return sortStatus.direction === "desc" ? sorted.reverse() : sorted;
     });
-    setRecords(sortBy(filteredRecords, sortStatus.columnAccessor));
-  }, [query]);
-
-  useEffect(() => {
-    const data = sortBy(processes, sortStatus.columnAccessor);
-    setRecords(sortStatus.direction === "desc" ? data.reverse() : data);
-  }, [sortStatus]);
+  }, [query, processes, sortStatus]);
 
   return (
     <PageWrapper name="Processes">
@@ -54,7 +62,7 @@ const ProcessesPage = () => {
         onChange={(e) => setQuery(e.currentTarget.value)}
       />
       <ProcessesTable
-        records={records}
+        records={records ?? []}
         selectedProcess={selectedProcess}
         setSelectedProcess={setSelectedProcess}
         setRecords={setRecords}
