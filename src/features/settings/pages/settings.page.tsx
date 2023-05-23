b17dfe5d117dc9c0a008d237aa67e2019de089e3
pacholoamit <pacholoamit.tech@gmail.com>
2023-05-23 15:06:44 +0800

    add mdx

--- src/features/settings/pages/settings.page.tsx
+++ src/features/settings/pages/settings.page.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import AutoStartSettingsView from "@/features/settings/views/autostart.view";
+import AboutView from "@/features/settings/views/about.view";
 import PageWrapper from "@/components/page-wrapper";
 import Card from "@/components/card";
 
-import { Icon24Hours } from "@tabler/icons-react";
+import { Icon24Hours, IconGitBranch } from "@tabler/icons-react";
 import { Center, Grid, NavLink } from "@mantine/core";
 import { useState } from "react";
 
-const settings = [{ icon: Icon24Hours, label: "Auto Start", view: <AutoStartSettingsView /> }];
+const settings = [
+  { icon: Icon24Hours, label: "Auto Start", view: <AutoStartSettingsView /> },
+  {
+    icon: IconGitBranch,
+    label: "About",
+    view: <AboutView />,
+  },
+];
 
 const SettingsPage = () => {
   const [active, setActive] = useState(0);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, optimizeDeps } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  optimizeDeps: {
+    include: ["react/jsx-runtime"],
+  },
+  plugins: [
+    react(),
+
+    mdx({
+      remarkPlugins: [],
+    }),
+  ],
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
   },
