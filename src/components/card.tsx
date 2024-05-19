0b726e8727b71c5c548b2b2ec5718ace9e3fc7c0
pacholoamit <pacholoamit.tech@gmail.com>
2024-05-20 00:21:02 +0800

    feat: Add DiskInformationAnalyticsCard component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ children, style, height = "300px" }) => {
-  const styles: React.CSSProperties = { height, ...style };
+  const styles: React.CSSProperties = { height, padding: 16, ...style };
   return (
     <MantineCard style={styles} shadow="xl" p="sm" radius={"md"} withBorder>
       {children}
--- src/features/metrics/pages/disk-analytics.page.tsx
+++ src/features/metrics/pages/disk-analytics.page.tsx
@@ -2,10 +2,77 @@ import useServerEventsContext from "@/hooks/useServerEventsContext";
 import PageWrapper from "@/components/page-wrapper";
 import DiskNotFound from "@/features/metrics/components/disks/disk.notfound";
 import Card from "@/components/card";
-import { Text, Stack, Title, Center, Container, Button, Grid } from "@mantine/core";
+import {
+  Text,
+  Stack,
+  Title,
+  Center,
+  Container,
+  Button,
+  Grid,
+  Group,
+  Badge,
+  Space,
+  DefaultMantineColor,
+} from "@mantine/core";
 import { Disk, commands } from "@/lib";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import formatBytes from "../utils/format-bytes";
+
+const DiskInformationAnalyticsCard = ({ disk }: { disk: Disk }) => {
+  const data: { label: string; value: string; color: DefaultMantineColor }[] = [
+    {
+      label: "Disk Type",
+      value: disk.diskType,
+      color: "blue",
+    },
+    {
+      label: "File System",
+      value: disk.fileSystem,
+      color: "cyan",
+    },
+    {
+      label: "Removable",
+      value: disk.isRemovable ? "Yes" : "No",
+      color: "lime",
+    },
+    {
+      label: "Free Space",
+      value: formatBytes(disk.free),
+      color: "green",
+    },
+    {
+      label: "Used Space",
+      value: formatBytes(disk.used),
+      color: "red",
+    },
+    {
+      label: "Total Space",
+      value: formatBytes(disk.total),
+      color: "gray",
+    },
+  ];
+
+  return (
+    <Card>
+      <Title order={4}>Disk Information</Title>
+      <Space h={8} />
+      <Stack spacing={3}>
+        {data.map((d, i) => (
+          <Group key={i} position="apart">
+            <Text c="dimmed" size={"sm"}>
+              {d.label}
+            </Text>
+            <Badge size="sm" variant="light" color={d.color}>
+              {d.value}
+            </Badge>
+          </Group>
+        ))}
+      </Stack>
+    </Card>
+  );
+};
 
 interface DiskAnalyticsPageProps {}
 
@@ -27,6 +94,9 @@ const DiskAnalyticsPage: React.FC<DiskAnalyticsPageProps> = () => {
   const { id = "" } = useParams();
   const [disk, setDisk] = React.useState<Disk>(defaultDisk);
 
+  // Stop if no disk id
+  if (!id) return <DiskNotFound />;
+
   useEffect(() => {
     if (!disks) return;
     const disk = disks.find((d) => d.id === id);
@@ -43,22 +113,15 @@ const DiskAnalyticsPage: React.FC<DiskAnalyticsPageProps> = () => {
     });
   };
 
-  if (!id) return <DiskNotFound />;
-
   return (
     <PageWrapper name={id}>
       <Grid>
         <Grid.Col span={3}>
-          <Card>
-            <Title order={4}>Disk Information</Title>
-            <Button onClick={() => onCheckDisk(disk.mountPoint)} color="blue">
-              Click
-            </Button>
-          </Card>
+          <DiskInformationAnalyticsCard disk={disk} />
         </Grid.Col>
         <Grid.Col span={9}>
           <Card>
-            <Title order={4}>Disk Info</Title>
+            <Title order={4}>Disk Analysis</Title>
           </Card>
         </Grid.Col>
       </Grid>
