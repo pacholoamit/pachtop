adc201aa8aeea3ed3dabed3e850c5c7e02762dc2
pacholoamit <pacholoamit.tech@gmail.com>
2024-05-19 23:47:48 +0800

    feat: Add DiskNotFound component for disk not found scenario

---
+++ src/features/metrics/components/disks/disk.notfound.tsx
@@ -0,0 +1,37 @@
+import { Container, Stack, Title, Text } from "@mantine/core";
+
+// TODO: maybe make this more generic later to be reused in other places?
+const DiskNotFound = () => {
+  return (
+    <Container pt={"30vh"}>
+      <Stack spacing="xl" align="center" justify="center">
+        <Title
+          order={1}
+          style={{
+            textAlign: "center",
+            fontWeight: 900,
+            fontSize: "3rem",
+          }}
+        >
+          Disk not found
+        </Title>
+        <Title
+          size="xl"
+          ta="center"
+          style={{
+            textAlign: "center",
+            fontWeight: 900,
+            fontSize: "2rem",
+          }}
+        >
+          You have found a secret place.
+        </Title>
+        <Text c="dimmed" size="xl" ta="center">
+          There was unfortunately no disk found at this location.
+        </Text>
+      </Stack>
+    </Container>
+  );
+};
+
+export default DiskNotFound;
--- src/features/metrics/pages/disk-analytics.page.tsx
+++ src/features/metrics/pages/disk-analytics.page.tsx
@@ -1,13 +1,14 @@
-import { Disk, commands } from "@/lib";
+import { commands } from "@/lib";
 import React from "react";
 import { useParams } from "react-router-dom";
+import PageWrapper from "@/components/page-wrapper";
+import DiskNotFound from "@/features/metrics/components/disks/disk.notfound";
+import { Text, Stack, Title, Center, Container } from "@mantine/core";
 
 interface DiskAnalyticsPageProps {}
 
 const DiskAnalyticsPage: React.FC<DiskAnalyticsPageProps> = () => {
-  const { id } = useParams();
-
-  console.log(id);
+  const { id = "" } = useParams();
 
   const onCheckDisk = async (mountPoint: string) => {
     console.log("Checking disk", mountPoint);
@@ -15,7 +16,14 @@ const DiskAnalyticsPage: React.FC<DiskAnalyticsPageProps> = () => {
       console.log(res);
     });
   };
-  return <div>TODO: DiskAnalyticsPage {id}</div>;
+
+  if (!id) return <DiskNotFound />;
+
+  return (
+    <PageWrapper name={id}>
+      <div>TODO: DiskAnalyticsPage {id}</div>
+    </PageWrapper>
+  );
 };
 
 export default DiskAnalyticsPage;
