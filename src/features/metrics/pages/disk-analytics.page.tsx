7f2e462bb1c925761a8e34254b0862d05470cd39
pacholoamit <pacholoamit.tech@gmail.com>
2024-05-19 23:55:07 +0800

    feat: Add default disk and fetch disk data on mount

--- src/features/metrics/pages/disk-analytics.page.tsx
+++ src/features/metrics/pages/disk-analytics.page.tsx
@@ -1,14 +1,39 @@
-import { commands } from "@/lib";
-import React from "react";
+import { Disk, commands } from "@/lib";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import PageWrapper from "@/components/page-wrapper";
 import DiskNotFound from "@/features/metrics/components/disks/disk.notfound";
 import { Text, Stack, Title, Center, Container } from "@mantine/core";
+import useServerEventsContext from "@/hooks/useServerEventsContext";
 
 interface DiskAnalyticsPageProps {}
 
+const defaultDisk: Disk = {
+  diskType: "unknown",
+  fileSystem: "unknown",
+  free: 0,
+  isRemovable: false,
+  mountPoint: "unknown",
+  total: 0,
+  name: "unknown",
+  timestamp: BigInt(0),
+  used: 0,
+  usedPercentage: 0,
+};
+
 const DiskAnalyticsPage: React.FC<DiskAnalyticsPageProps> = () => {
+  const { disks } = useServerEventsContext();
   const { id = "" } = useParams();
+  const [disk, setDisk] = React.useState<Disk>(defaultDisk);
+
+  useEffect(() => {
+    if (!disks) return;
+    const disk = disks.find((d) => d.id === id);
+
+    if (disk?.data?.length ?? 0 > 0) {
+      setDisk(disk?.data?.at(-1) || defaultDisk);
+    }
+  }, [disks]);
 
   const onCheckDisk = async (mountPoint: string) => {
     console.log("Checking disk", mountPoint);
@@ -21,7 +46,7 @@ const DiskAnalyticsPage: React.FC<DiskAnalyticsPageProps> = () => {
 
   return (
     <PageWrapper name={id}>
-      <div>TODO: DiskAnalyticsPage {id}</div>
+      <div>TODO: DiskAnalyticsPage {disk.name}</div>
     </PageWrapper>
   );
 };
