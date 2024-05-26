cb2d3ef573c9c80531a1b0de86c10120f4e2a54a
pacholoamit <pacholoamit.tech@gmail.com>
2024-05-27 07:03:55 +0800

    style: Remove commented-out code and update imports

--- src/features/processes/pages/processes-revamp.page.tsx
+++ src/features/processes/pages/processes-revamp.page.tsx
@@ -14,8 +14,8 @@ import useProcessesSelectors from "@/features/processes/stores/processes.store";
 // @ts-ignore
 import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import { ColDef, ModuleRegistry } from "@ag-grid-community/core";
-import { ActionIcon, Grid, Group, Space, Text, TextInput } from "@mantine/core";
-import { IconCircleX } from "@tabler/icons-react";
+import { ActionIcon, Grid, Group, Space, Tabs, Text, TextInput } from "@mantine/core";
+import { IconCircleX, IconTable, IconTablePlus } from "@tabler/icons-react";
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
@@ -151,7 +151,92 @@ const ProcessesByDisk = () => {
   );
 };
 
+const ProcessesAll = () => {
+  const processes = useProcessesSelectors.use.processes();
+  console.log(processes);
+  const [columns, setColumns] = useState<ColDef[]>([
+    {
+      field: "name",
+      flex: 4,
+      filter: true,
+      sort: "asc",
+    },
+    {
+      field: "diskUsage.totalWrittenBytes",
+      flex: 4,
+      headerName: "Writes",
+      cellClass: "number",
+      valueFormatter: ({ value }) => formatBytes(value ?? 0),
+    },
+    {
+      field: "diskUsage.totalReadBytes",
+      flex: 4,
+      headerName: "Reads",
+      cellClass: "number",
+      valueFormatter: ({ value }) => formatBytes(value ?? 0),
+    },
+    {
+      field: "cpuUsage",
+      flex: 4,
+      headerName: "CPU Usage",
+      cellClass: "number",
+      cellRenderer: "agAnimateShowChangeCellRenderer",
+      valueFormatter: ({ value }) => fromNumberToPercentageString(value),
+    },
+
+    {
+      field: "memoryUsage",
+      flex: 4,
+      headerName: "RAM Usage",
+      cellClass: "number",
+      cellRenderer: "agAnimateShowChangeCellRenderer",
+      valueFormatter: ({ value }) => formatBytes(value),
+    },
+    {
+      field: "root",
+      flex: 3,
+      headerName: "Installed on",
+    },
+    {
+      field: "Actions",
+      cellRenderer: ActionsColumn,
+      flex: 2,
+    },
+  ]);
+  return (
+    <Card height="580px">
+      <Text>Processes by CPU Usage</Text>
+      <Space h={12} />
+      <div style={{ height: "100%", width: "100%" }} className="ag-theme-slate">
+        <AgGridReact rowData={processes} columnDefs={columns as any} />
+      </div>
+    </Card>
+  );
+};
+
+const MultiTables = () => {
+  return (
+    <Grid>
+      <Grid.Col xl={4} lg={6}>
+        <ProcessesByMemory />
+      </Grid.Col>
+      <Grid.Col xl={4} lg={6}>
+        <ProcessesByCPU />
+      </Grid.Col>
+      <Grid.Col xl={4} lg={6}>
+        <ProcessesByDisk />
+      </Grid.Col>
+    </Grid>
+  );
+};
+
+const TAB_OPTIONS = {
+  segmented: { value: "segmented", label: "Segmented", icon: <IconTablePlus /> },
+  all: { value: "all", label: "All", icon: <IconTable /> },
+};
+
 const ProcessesPage = () => {
+  const [isMultiTables, setIsMultiTables] = useState(false);
   return (
     <PageWrapper name="Processes">
       <Grid>
@@ -161,14 +246,20 @@ const ProcessesPage = () => {
         <Grid.Col xl={4} lg={6}>
           <ProcessesInsights />
         </Grid.Col>
-        <Grid.Col xl={4} lg={6}>
-          <ProcessesByMemory />
-        </Grid.Col>
-        <Grid.Col xl={4} lg={6}>
-          <ProcessesByCPU />
-        </Grid.Col>
-        <Grid.Col xl={4} lg={6}>
-          <ProcessesByDisk />
+        <Grid.Col span={12}>
+          <Tabs defaultValue={TAB_OPTIONS.segmented.value}>
+            <Tabs.List>
+              <Tabs.Tab color="teal" icon={TAB_OPTIONS.segmented.icon} value={TAB_OPTIONS.segmented.value} />
+              <Tabs.Tab color="red" icon={TAB_OPTIONS.all.icon} value={TAB_OPTIONS.all.value} />
+            </Tabs.List>
+            <Space h={20} />
+            <Tabs.Panel value={TAB_OPTIONS.segmented.value}>
+              <MultiTables />
+            </Tabs.Panel>
+            <Tabs.Panel value={TAB_OPTIONS.all.value}>
+              <ProcessesAll />
+            </Tabs.Panel>
+          </Tabs>
         </Grid.Col>
       </Grid>
     </PageWrapper>
