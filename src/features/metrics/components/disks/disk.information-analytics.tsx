3f56a05d9caea85476ef16f08e9e7fce25ea38e2
pacholoamit <pacholoamit.tech@gmail.com>
2024-05-30 10:49:49 +0800

    fix: finding disk for unix systems

--- src/features/metrics/components/disks/disk.information-analytics.tsx
+++ src/features/metrics/components/disks/disk.information-analytics.tsx
@@ -28,6 +28,8 @@ const DiskInformationAnalyticsCard = (props: DiskInformationAnalyticsCardProps) 
   const [opened, { close, open }] = useDisclosure(false);
   const disk = useDisksStore.use.selectedDisk();
 
+  console.log("chosen ", disk)
+
   const data: { label: string; value: string; color: DefaultMantineColor }[] = [
     {
       label: "Location",
