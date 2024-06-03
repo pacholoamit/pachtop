825ce018e2e537b0f7812acdbf8f078a4ed6b434
pacholoamit <pacholoamit.tech@gmail.com>
2024-06-03 17:04:29 +0800

    style: Remove unnecessary Space component

--- src/layout/components/navbar.tsx
+++ src/layout/components/navbar.tsx
@@ -4,7 +4,6 @@ import { Navbar as MantineNavbar, ScrollArea, Space } from "@mantine/core";
 const AppNavbar = () => {
   return (
     <MantineNavbar p={0} width={{ base: 50 }}>
-      <Space h={"xs"} />
       <MantineNavbar.Section grow component={ScrollArea} mx="-xs" px="xs">
         <NavbarOptions />
       </MantineNavbar.Section>
