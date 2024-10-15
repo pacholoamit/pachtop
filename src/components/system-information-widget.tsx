6b77764660447298ee54595f70713a9080ab7590
pacholoamit <pacholoamit.tech@gmail.com>
2024-10-15 10:02:47 +0800

    style: Update quotation marks to use double quotes

--- src/components/system-information-widget.tsx
+++ src/components/system-information-widget.tsx
@@ -1,12 +1,12 @@
-import { useShallow } from 'zustand/react/shallow';
+import { useShallow } from "zustand/react/shallow";
 
-import Card from '@/components/card';
-import useGlobalCpuSelectors from '@/features/metrics/stores/global-cpu.store';
-import useMemorySelectors from '@/features/metrics/stores/memory.store';
-import useSwapSelectors from '@/features/metrics/stores/swap.store';
-import useSystemStoreSelectors from '@/features/metrics/stores/system.store';
-import formatBytes from '@/features/metrics/utils/format-bytes';
-import { Grid, Group, Text, Title } from '@mantine/core';
+import Card from "@/components/card";
+import useGlobalCpuSelectors from "@/features/metrics/stores/global-cpu.store";
+import useMemorySelectors from "@/features/metrics/stores/memory.store";
+import useSwapSelectors from "@/features/metrics/stores/swap.store";
+import useSystemStoreSelectors from "@/features/metrics/stores/system.store";
+import formatBytes from "@/features/metrics/utils/format-bytes";
+import { Grid, Group, Text, Title } from "@mantine/core";
 
 const SystemInformationWidget: React.FC = () => {
   const info = useSystemStoreSelectors(useShallow((state) => state.info));
@@ -17,52 +17,52 @@ const SystemInformationWidget: React.FC = () => {
 
   return (
     <Card height="">
-      <Grid align="center" justify="center">
-        <Grid.Col md={1.5} sm={6}>
+      <Grid align={"baseline"} justify="space-between">
+        <Grid.Col md={2} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             CPU
           </Text>
           <Text size={"sm"}>{cpuBrand}</Text>
         </Grid.Col>
-        <Grid.Col md={1.5} sm={6}>
+        <Grid.Col md={1.3} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             OS
           </Text>
           <Text size={"sm"}>{info.os}</Text>
         </Grid.Col>
-        <Grid.Col md={1.5} sm={6}>
+        <Grid.Col md={1.3} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             Arch
           </Text>
           <Text size={"sm"}>{info.arch}</Text>
         </Grid.Col>
-        <Grid.Col md={1.5} sm={6}>
+        <Grid.Col md={1.3} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             CPU cores
           </Text>
           <Text size={"sm"}>{info.coreCount}</Text>
         </Grid.Col>
 
-        <Grid.Col md={1.5} sm={6}>
+        <Grid.Col md={1.3} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             Kernel
           </Text>
           <Text size={"sm"}>{info.kernelVersion}</Text>
         </Grid.Col>
-        <Grid.Col md={1.5} sm={6}>
+        <Grid.Col md={1.3} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             Memory
           </Text>
           <Text size={"sm"}>{formatBytes(memory)}</Text>
         </Grid.Col>
-        <Grid.Col md={1.5} sm={6}>
+        <Grid.Col md={1.3} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             Swap
           </Text>
           <Text size={"sm"}>{formatBytes(swap)}</Text>
         </Grid.Col>
 
-        <Grid.Col md={1.5} sm={6}>
+        <Grid.Col md={1.3} sm={6}>
           <Text c="dimmed" size="sm" tt="uppercase" fw={700}>
             Uptime
           </Text>
